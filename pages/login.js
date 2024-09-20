ee82db86279fae7a7fd4646e1b690ec1dd37d29c
Shakeeey <136384872+Shakeeey@users.noreply.github.com>
2024-09-19 22:44:54 -0500

    Edited Pages and fixed packages

---
+++ pages/login.js
@@ -0,0 +1,14 @@
+// pages/login.js
+import React from 'react';
+import LoginForm from '../components/LoginForm';
+
+const LoginPage = () => {
+  return (
+    <div className="container">
+      <h1>Login</h1>
+      <LoginForm />
+    </div>
+  );
+};
+
+export default LoginPage;
---
+++ pages/register.js
@@ -0,0 +1,14 @@
+// pages/register.js
+import React from 'react';
+import RegistrationForm from '../components/RegistrationForm';
+
+const RegisterPage = () => {
+  return (
+    <div className="container">
+      <h1>Register</h1>
+      <RegistrationForm />
+    </div>
+  );
+};
+
+export default RegisterPage;
